86f4578c4b85404282053eaecb940e9caf3c2011
Howard M. Lewis Ship <hlship@walmartlabs.com>
2016-06-24 14:23:15 -0700

    Upgrade to latest dependencies

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject walmartlabs/active-status "0.1.6"
+(defproject walmartlabs/active-status "0.1.7"
   :description "Present status of mulitple 'jobs' in a command line tool, using ANSI escape codes"
   :url "https://github.com/walmartlabs/active-status"
   :license {:name "Apache Sofware License 2.0"
             :url  "http://www.apache.org/licenses/LICENSE-2.0.html"}
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.async "0.2.374"]
-                 [medley "0.8.1"]
+                 [org.clojure/core.async "0.2.385"]
+                 [medley "0.8.2"]
                  [io.aviso/toolchest "0.1.4"]
                  [io.aviso/pretty "0.1.26"]
                  [io.aviso/config "0.1.13" :optional true]]
